4b7338118e7ca8fec9f6b56c48208792352007dc
“Brijesh” <brijeshkulal5@gmail.com>
2021-11-09 20:55:51 +0530

    Added welcome message

---
+++ src/main/java/com/census/analyzer/IndianStatesCensusAnalyser.java
@@ -0,0 +1,8 @@
+package com.census.analyzer;
+
+public class IndianStatesCensusAnalyser {
+	public static void main(String[] args) {
+		System.out.println("Welcome to indian state census analyser problem");
+	}
+}
+
